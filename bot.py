90199ec016be939b70120089d4108211e3244df2
CocoTheOwner <65736624+CocoTheOwner@users.noreply.github.com>
2021-02-09 18:38:58 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,13 +11,23 @@ import aiohttp
 from unidecode import unidecode
 from difflib import get_close_matches
 
-# Import subprocess
+# Toggle wiki command & functionality on or off
+use_wiki_commands = True # toggle
+use_wikis = ["example", "iris", "vehiclesplus", "react"]  # specific toggle (included => on)
+if use_wiki_commands:
+    try:
+        import wiki as wikilib
+        wikilib.toggle_keys(use_wikis)
+    except ImportError:
+        print("Unable to import wiki functions. Is the file in your directory?")
 
+# Import subprocess
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.default(),
                    case_insensitive=True)
 
 load_dotenv()
-token = os.getenv('token')
+#token = os.getenv('token')
+token = "ODAyNDQ4MDM0MjkzNzQzNjY5.YAvX-A.qHKVCjZ83CBWuJd6zL12DApmqjg"
 
 logging.basicConfig(filename='console.log',
                     level=logging.INFO,
@@ -25,171 +35,6 @@ logging.basicConfig(filename='console.log',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     )
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-
-"""
-    Wiki definitions
-    'wikis' contains number to function name mappings.
-    'wikialts' contains name to number mappings.
-    The dict keys in 'wikialts' are approximations to the inputted command.
-    We later search for the closest match in the dictionary.
-    e.g. 'example' points to 'wiki_example(*defs)' through wikialts and then wikis 
-
-    If you want to delete a plugin, remove its respective entries from 'wikis' and 'wikialts'.
-    It does not work if you only delete the index registry under wiki_<name>_pages
-"""
-wikis = {
-    # Name as in function (e.g. wiki_example() )
-    -1: 'example',
-    0: 'iris',
-    1: 'vehiclesplus',
-    2: 'react'
-}
-wikialts = {
-    # Example alts
-    'example': -1,
-    'exam': -1,
-    'ex': -1,
-
-    # Iris alts
-    'iris': 0,
-    'ir': 0,
-    'iri': 0,
-    'i': 0,
-
-    # VehiclesPlus alts
-    'vehiclesplus': 1,
-    'vp': 1,
-    'vehclesplus': 1,
-
-    # React alts
-    'react': 2,
-    're': 2,
-    'ract': 2,
-    'rea': 2
-}
-
-"""
-    _path describes the main directory path
-    _pages is a dictionary of all subsequent pages.
-        Keys should be the title of the page
-        Make sure to define "main" at least.
-"""
-wiki_example_path = 'https://docs.gitbook.com/'
-wiki_example_pages = {
-    "start exploring": "getting-started/start-exploring", # Real page for Gitbook
-    "main": "" # Main entry also points to main page
-}
-
-wiki_iris_path = 'https://volmitsoftware.gitbook.io/iris/'
-wiki_iris_pages = {
-    # Main
-    "main": "",
-    "intro": "",
-
-    "getting started": "getting-started",
-    "get started": "getting-started",
-    "started": "getting-started",
-
-
-
-    # Plugin category
-    "commands": "plugin/commands",
-    "cmd": "plugin/commands",
-
-    "permissions": "plugin/permissions",
-    "perms": "plugin/permissions",
-
-    "configuration": "plugin/configuration",
-    "config": "plugin/configuration",
-
-    "faq": "plugin/faq",
-    "frequently asked questions": "plugin/faq",
-
-
-
-    # Engine category
-    "introduction to the engine": "engine/introduction-to-the-engine",
-    "engine intro": "engine/introduction-to-the-engine",
-    "introduction": "engine/introduction-to-the-engine",
-
-    "studio": "engine/studio",
-    "studio intro": "engine/studio",
-    "std": "engine/studio",
-
-    "creating a new project": "engine/new-project",
-    "new project": "engine/new-project",
-    "project": "engine/new-project",
-
-    "understanding": "engine/understanding",
-    "understand": "engine/understanding",
-        # Understanding subcategory
-
-        "dimension": "engine/understanding/dimensions",
-        "dimensions": "engine/understanding/dimensions",
-        "dim": "engine/understanding/dimensions",
-
-        "region": "engine/understanding/regions",
-        "regions": "engine/understanding/regions",
-        "reg": "engine/understanding/regions",
-
-        "biome": "engine/understanding/biomes",
-        "biomes": "engine/understanding/biomes",
-        "bio": "engine/understanding/biomes",
-        
-        "generator": "engine/understanding/generators",
-        "generators": "engine/understanding/generators",
-        "gen": "engine/understanding/generators",
-        
-        "object": "engine/understanding/objects",
-        "objects": "engine/understanding/objects",
-        "obj": "engine/understanding/objects",
-        
-        "jigsaw": "engine/understanding/jigsaw",
-        "jig": "engine/understanding/jigsaw",
-        
-        "loot": "engine/understanding/loot",
-        
-        "entity": "engine/understanding/entities",
-        "entities": "engine/understanding/entities",
-        "ent": "engine/understanding/entities",
-
-    "mastering": "engine/mastering",
-    "master": "engine/mastering",
-        # Mastering subcategory
-
-        "mastering dimension": "engine/mastering/dimensions",
-        "mastering dimensions": "engine/mastering/dimensions",
-        "mastering dim": "engine/mastering/dimensions",
-
-        "mastering biome": "engine/mastering/biomes",
-        "mastering biomes": "engine/mastering/biomes",
-        "mastering bio": "engine/mastering/biomes",
-
-        "mastering universal": "engine/mastering/universal-parameters",
-        "universal": "engine/mastering/universal-parameters",
-        "universal parameters": "engine/mastering/universal-parameters",
-        "master universal": "engine/mastering/universal-parameters",
-
-    "packaging and publishing dimension": "engine/packaging-and-publishing",
-    "packaging and publishing": "engine/packaging-and-publishing",
-    "package and publish": "engine/packaging-and-publishing",
-    "package and publish dimension": "engine/packaging-and-publishing",
-    "publish dimension": "engine/packaging-and-publishing",
-    "upload dimension": "engine/packaging-and-publishing",
-
-    "updating the overworld": "engine/updateoverworld",
-    "update overworld": "engine/updateoverworld",
-    "updating overworld": "engine/updateoverworld"}
-
-wiki_react_path = 'https://volmitsoftware.gitbook.io/react/'
-wiki_react_pages = {
-    "main": "" # Main entry also points to main page
-}
-
-wiki_vehiclesplus_path = 'https://volmitsoftware.gitbook.io/vehiclesplus/'
-wiki_vehiclesplus_pages = {
-    "main": "" # Main entry also points to main page
-}
 
 @bot.event
 async def on_ready():
@@ -203,32 +48,30 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     # Binflop
-    if len(message.attachments) > 0:
-        if not message.attachments[0].url.endswith(
-                ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image', '.svg')):
-            download = message.attachments[0].url
-            async with aiohttp.ClientSession() as session:
-                async with session.get(download, allow_redirects=True) as r:
+    if len(message.attachments) > 0 and not message.attachments[0].url.endswith(
+        ('.png', '.jpg', '.jpeg', '.mp4', '.mov', '.avi', '.gif', '.image', '.svg')):
+        download = message.attachments[0].url
+        async with aiohttp.ClientSession() as session:
+            async with session.get(download, allow_redirects=True) as r:
 
-                    # r = requests.get(download, allow_redirects=True)
-                    text = await r.text()
-                    text = unidecode(text)
-                    text = "\n".join(text.splitlines())
-                    if '�' not in text:  # If it's not an image/gif
-                        truncated = False
-                        if len(text) > 100000:
-                            text = text[:99999]
-                            truncated = True
-                        req = requests.post('https://bin.bloom.host/documents', data=text)
-                        key = json.loads(req.content)['key']
-                        response = ""
-                        response = response + "https://bin.bloom.host/" + key
-                        response = response + "\nRequested by " + message.author.mention
-                        if truncated:
-                            response = response + "\n(file was truncated because it was too long.)"
-                        embed_var = discord.Embed(title="Please use a paste service", color=0x1D83D4)
-                        embed_var.description = response
-                        await message.channel.send(embed=embed_var)
+                #. r = requests.get(download, allow_redirects=True)
+                text = await r.text()
+                text = unidecode(text)
+                text = "\n".join(text.splitlines())
+                if '�' not in text:  # If it's not an image/gif
+                    truncated = False
+                    if len(text) > 100000:
+                        text = text[:99999]
+                        truncated = True
+                    req = requests.post('https://bin.bloom.host/documents', data=text)
+                    key = json.loads(req.content)['key']
+                    response = "https://bin.bloom.host/" + key
+                    response += "\nRequested by " + message.author.mention
+                    if truncated:
+                        response += "\n(file was truncated because it was too long.)"
+                    embed_var = discord.Embed(title="Please use a paste service", color=0x1D83D4)
+                    embed_var.description = response
+                    await message.channel.send(embed=embed_var)
     timings = bot.get_cog('Timings')
     await timings.analyze_timings(message)
     await bot.process_commands(message)
@@ -236,6 +79,11 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Kahti bot ping is {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def wiki(ctx, *args):
+    if use_wiki_commands:
+        await wikilib.wiki(ctx, *args)
 
 @bot.command()
 async def invite(ctx):
